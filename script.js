34535559d133076bd4225b6266304768d44c66dd
Alberto-Ricardo <jossealbertoricardo@gmail.com>
2023-03-25 00:00:44 -0600

    First Commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+function getVerse(t) {
+
+	var b = document.getElementById('libro');
+var book = b.value;
+
+var c = document.getElementById('capitulo');
+var chapter = c.value;
+
+var v = document.getElementById('versiculo');
+var verse = v.value;
+
+var chunk = "https://bible-api.com/";
+var plus = "+";
+var colon = ":";
+var target = chunk + book + plus + chapter + colon + verse;
+console.log(target);
+
+	fetch(target)
+		.then(response => response.json())
+		.then(response => {
+			var reference = response.reference;
+			var texto = response.text;
+
+			var resultado = document.getElementById('resultado');
+			resultado.innerHTML = '<p>' + texto + '</p><p style="text-align: right">' + reference + '</p>';
+		})
+		
+		.catch(err => console.log(err))
+
+
+};
+
+fetch('https://bible-api.com/Luke+6:36 ')
+    .then(response => response.json())
+	.then(response => console.log(response.text))
+	.catch(err => console.log(err))
+// ;)
